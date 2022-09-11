4b627eb7d9348d6ee29d8f3cae7eda95e3525ac2
Iain Cambridge <iain@humblyarrogant.io>
2022-09-11 12:45:49 +0200

    Update

--- publish.sh
+++ publish.sh
@@ -1,7 +1,9 @@
-cp -R docs ../parthenon.com
-cp -R version_docs ../parthenon.com
+git clone git@github.com:getparthenon/getparthenon.com.git ../getparthenon.com
 
-cd ../parthenon.com
+cp -R docs ../getparthenon.com
+cp -R version_docs ../getparthenon.com
+
+cd ../getparthenon.com
 
 if [ -z "$(git status --porcelain)" ]; then
   echo "No changes found"
