6bb9298b53618774d7d6a4b63fa49a29feb6912b
Iain Cambridge <iain@humblyarrogant.io>
2022-09-21 13:45:25 +0200

    Update docs

--- publish.sh
+++ publish.sh
@@ -1,5 +1,8 @@
 git clone git@github.com:getparthenon/getparthenon.com.git ../getparthenon.com
 
+rm -Rf ../getparthenon.com/docs/*
+rm -Rf ../getparthenon.com/version_docs/*
+
 cp -R docs ../getparthenon.com
 cp -R version_docs ../getparthenon.com
 
